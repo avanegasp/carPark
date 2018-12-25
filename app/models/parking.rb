986ad4a3b9c22f1193711caa1b5fc84c041a93df
Angie <arco527@gmail.com>
2018-12-24 19:57:21 -0500

    Show vehicles and parking

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,3 +1,6 @@
 class Parking < ApplicationRecord
   belongs_to :vehicle
+  validates :rate, numericality: true
+  validates :arrival_time, numericality: true
+  validates :exit_time, numericality: true
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,5 @@
 class Vehicle < ApplicationRecord
   has_many :parkings, dependent: :destroy
-  validates :plate, presence: true
+  validates_format_of :plate, :with => /[A-Z]{3}\d\d\d/
+  validates :vehicle_type, presence: true
 end
