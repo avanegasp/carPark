2e78c3181dbadd889d9e1867479ce2c4fb7e0783
Angie <arco527@gmail.com>
2019-09-24 18:52:23 -0500

    Card

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,12 +1,12 @@
 class ParkingsController < ApplicationController
 
-def index
-  @parkings = Parking.all
-  @vehicle = Vehicle.new
-  @vehicles = Vehicle.all
-end
+  def index
+    @parkings = Parking.all
+    @vehicle = Vehicle.new
+    @vehicles = Vehicle.all
+  end
 
-def show
-  @parking = Parking.find(params[:id])
-end
+  def show
+    @parking = Parking.find(params[:id])
+  end
 end
